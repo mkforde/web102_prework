55b0845828ab01d1dc298625391f98673d7653ff
Michael Forde <michael.kenneth.forde@gmail.com>
2025-01-07 20:05:12 -0500

    Complete Challenge 5

--- index.js
+++ index.js
@@ -90,8 +90,12 @@ function filterUnfundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have not yet met their goal
+  let listOfUnfoundedGames = GAMES_JSON.filter((game) => {
+    return game.goal > game.pledged;
+  });
 
   // use the function we previously created to add the unfunded games to the DOM
+  addGamesToPage(listOfUnfoundedGames);
 }
 
 // show only games that are fully funded
@@ -99,8 +103,12 @@ function filterFundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have met or exceeded their goal
+  let listOfFoundedGames = GAMES_JSON.filter((game) => {
+    return game.goal < game.pledged;
+  });
 
   // use the function we previously created to add unfunded games to the DOM
+  addGamesToPage(listOfFoundedGames);
 }
 
 // show all games
@@ -108,6 +116,7 @@ function showAllGames() {
   deleteChildElements(gamesContainer);
 
   // add all games from the JSON data to the DOM
+  addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -116,6 +125,13 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", () => {
+  filterUnfundedOnly();
+});
+
+fundedBtn.addEventListener("click", () => filterFundedOnly());
+
+allBtn.addEventListener("click", () => showAllGames());
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
