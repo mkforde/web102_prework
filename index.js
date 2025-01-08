22e0f829e7156b2454aaeb1790cfecff5ea432a1
Michael Forde <michael.kenneth.forde@gmail.com>
2025-01-07 19:36:43 -0500

    Complete Challenge 4

--- index.js
+++ index.js
@@ -62,8 +62,7 @@ const totalContributions = GAMES_JSON.reduce((acc, game) => {
   return acc + game.backers;
 }, 0);
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-contributionsCard.innerHTML = `<h4>${totalContributions.toLocaleString("en-US")}
-</h4>`;
+contributionsCard.innerHTML = `${totalContributions.toLocaleString("en-US")}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
@@ -72,9 +71,13 @@ const totalRaised = GAMES_JSON.reduce((acc, game) => {
   return acc + game.pledged;
 }, 0);
 // set inner HTML using template literal
+raisedCard.innerHTML = `$${totalRaised.toLocaleString("en-US")}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+const totalGames = GAMES_JSON.length;
+gamesCard.innerHTML = `${totalGames}`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
